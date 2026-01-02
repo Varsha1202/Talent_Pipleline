d39201ad7d0fedcaa05ee114e57b337adde4ac49
Varsha <91330320+Varsha1202@users.noreply.github.com>
2026-01-02 21:28:09 +0530

    Add CandidateProfileResponse DTO class

---
+++ src/main/CandidateProfile/dto/CandidateProfileResponse.java
@@ -0,0 +1,52 @@
+package com.example.CandidateProfile.dto;
+
+
+public class CandidateProfileResponse {
+
+    private Long candidateId;
+    private String resumeUrl;
+    private Double experienceYears;
+    private String education;
+    private String skills;
+    private String portfolioLink;
+
+    public CandidateProfileResponse(
+            Long candidateId,
+            String resumeUrl,
+            Double experienceYears,
+            String education,
+            String skills,
+            String portfolioLink
+    ) {
+        this.candidateId = candidateId;
+        this.resumeUrl = resumeUrl;
+        this.experienceYears = experienceYears;
+        this.education = education;
+        this.skills = skills;
+        this.portfolioLink = portfolioLink;
+    }
+
+    public Long getCandidateId() {
+        return candidateId;
+    }
+
+    public String getResumeUrl() {
+        return resumeUrl;
+    }
+
+    public Double getExperienceYears() {
+        return experienceYears;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public String getPortfolioLink() {
+        return portfolioLink;
+    }
+}
